d91f0aa0892360974807b7d2972169efb8cc2bb1
je-an <mail@jeremiasandris.de>
2020-08-15 16:21:13 +0200

    Fixed bugs in templates

--- generators/app/templates/src/_.js
+++ generators/app/templates/src/_.js
@@ -1,23 +1,25 @@
 define(
     [
-        "jQuery",
-        "TypeCheck", 
-        "Inheritance", 
-        "Control",
+        "DomElement",
+        "DomUtil",
+        "Inheritance",
+        "TypeCheck",
+        "Failure",
+        "Merge",
+        "text!<%= constructorLowerCase %>-html"
         "css!<%= constructorLowerCase %>-css"
-    ], function ($, TypeCheck, Inheritance, Control) {
-    /**
-     * <%= description %> 
-     * @alias <%= constructor %> 
-     * @constructor
-     * @param {Object} options - options object
-     */
-    var <%= constructor %> = function(options) {
-        var defaultOptions = {};
-        Inheritance.inheritConstructor(Control, this, [options, defaultOptions]);
-        // Pass your html markup this._element
-        this._element = $("<div id='<%= name %>'></div>");
-    };
-    Inheritance.inheritPrototype(<%= constructor %>, Control);
-    return <%= constructor %>;
-});
+    ], function (DomElement, DomUtil, Inheritance, TypeCheck, Failure, Merge) {
+        /**
+         * <%= description %> 
+         * @alias <%= constr %> 
+         * @constructor
+         * @param {Object} options - options object
+         */
+        var <%= constr %> = function(options) {
+            var defaultOptions = {};
+            Inheritance.inheritConstructor(DomElement, this, Merge({}, TypeCheck.isDefined(options) ? options : {}));
+            this._element = $("<div id='<%= name %>'></div>");
+        };
+        Inheritance.inheritPrototype(<%= constr %>, DomElement);
+        return <%= constr %>;
+    });
